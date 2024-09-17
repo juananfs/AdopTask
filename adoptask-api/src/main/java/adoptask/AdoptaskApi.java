3a1c257cc4fb76256b21b198d0fb03547fba1e35
Juani <Juani@host.docker.internal>
2024-09-17 11:28:29 +0200

    Creado proyecto para la API

---
+++ adoptask-api/src/main/java/adoptask/AdoptaskApi.java
@@ -0,0 +1,13 @@
+package adoptask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaskApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaskApi.class, args);
+	}
+
+}
---
+++ adoptask-api/src/test/java/adoptask/AdoptaskApiTests.java
@@ -0,0 +1,13 @@
+package adoptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaskApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
