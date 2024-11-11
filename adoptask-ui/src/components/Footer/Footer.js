22c257bc7bbf11c86afe40249da23d044ca6d394
juanantonio-fernandez-soriano-um-es <juanantonio.fernandezs@um.es>
2024-11-11 17:51:13 +0100

    Creado Footer

---
+++ adoptask-ui/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css';
+import Text from './Text';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <Text />
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ adoptask-ui/src/components/Footer/Text.js
@@ -0,0 +1,9 @@
+function Text() {
+    return (
+        <p>
+            &copy; {new Date().getFullYear()} AdopTask - Creado por Juan Antonio FS
+        </p>
+    );
+}
+
+export default Text;
