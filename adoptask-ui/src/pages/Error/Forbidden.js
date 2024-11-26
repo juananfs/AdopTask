d40946cd8a67ae5af86fa4237542362e8ac6c407
juanantonio-fernandez-soriano-um-es <juanantonio.fernandezs@um.es>
2024-11-26 18:46:24 +0100

    Corrección páginas de error

--- adoptask-ui/src/pages/Error/Forbidden.js
+++ adoptask-ui/src/pages/Error/Forbidden.js
@@ -3,10 +3,10 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
 const Forbidden = () => (
-    <div className='error-page text-center'>
+    <div className='error-page'>
         <Header />
-        <h2 className='mt-5'>403 - Prohibido</h2>
-        <p>Lo sentimos, no tiene permiso para acceder a esta página.</p>
+        <h2 className='mt-5 text-center'>403 - Prohibido</h2>
+        <p className='text-center'>Lo sentimos, no tiene permiso para acceder a esta página.</p>
         <Footer />
     </div>
 );
--- adoptask-ui/src/pages/Error/NotFound.js
+++ adoptask-ui/src/pages/Error/NotFound.js
@@ -3,10 +3,10 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
 const NotFound = () => (
-    <div className='error-page text-center'>
+    <div className='error-page'>
         <Header />
-        <h2 className='mt-5'>404 - Página no encontrada</h2>
-        <p>Lo sentimos, la página que estás buscando no existe.</p>
+        <h2 className='mt-5 text-center'>404 - Página no encontrada</h2>
+        <p className='text-center'>Lo sentimos, la página que estás buscando no existe.</p>
         <Footer />
     </div>
 );
